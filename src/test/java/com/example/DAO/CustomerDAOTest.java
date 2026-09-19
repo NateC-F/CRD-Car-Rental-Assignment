db54124e458b359a4e7dbf26abb003a40d36ee32
NateC-F <148012685+NateC-F@users.noreply.github.com>
2026-09-19 19:59:58 -0400

    Unit Tested CustomerDAO

--- src/test/java/com/example/DAO/CustomerDAOTest.java
+++ src/test/java/com/example/DAO/CustomerDAOTest.java
@@ -1,7 +1,69 @@
 package com.example.DAO;
 
+import com.example.Model.Customer;
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class CustomerDAOTest
 {
     //DAO Create functions were tested separately
 
+    @Test
+    void testGetCustomerIdFromLicense() throws SQLException
+    {
+        CustomerDAO customerDAO = new CustomerDAO();
+
+        int customerId = customerDAO.getCustomerIdFromLicense("TEST001");
+
+        assertTrue(customerId == 7);
+    }
+
+    @Test
+    void testGetCustomerFromDatabaseId() throws SQLException
+    {
+        CustomerDAO customerDAO = new CustomerDAO();
+
+        int customerId = customerDAO.getCustomerIdFromLicense("TEST001");
+
+        Customer customer = customerDAO.getCustomerFromDatabaseId(customerId);
+
+        assertNotNull(customer);
+        assertEquals("Test Customer", customer.getCustomerName());
+        assertEquals("TEST001", customer.getCustomerLicense());
+    }
+
+    @Test
+    void testGetCustomerFromInvalidDatabaseId() throws SQLException
+    {
+        CustomerDAO customerDAO = new CustomerDAO();
+
+        Customer customer = customerDAO.getCustomerFromDatabaseId(-1);
+
+        assertNull(customer);
+    }
+
+    @Test
+    void testLookUpCustomerByLicense() throws SQLException
+    {
+        CustomerDAO customerDAO = new CustomerDAO();
+
+        Customer customer = customerDAO.lookUpCustomerByLicense("TEST001");
+
+        assertNotNull(customer);
+        assertEquals("Test Customer", customer.getCustomerName());
+        assertEquals("TEST001", customer.getCustomerLicense());
+    }
+
+    @Test
+    void testLookUpCustomerByInvalidLicense() throws SQLException
+    {
+        CustomerDAO customerDAO = new CustomerDAO();
+
+        Customer customer = customerDAO.lookUpCustomerByLicense("DOESNOTEXIST");
+
+        assertNull(customer);
+    }
 }
